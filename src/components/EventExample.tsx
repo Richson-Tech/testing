dbf2253f608cf50eeb46943dc645db622f80034f
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-05-08 19:42:06 +0100

    new commits

---
+++ src/components/EventExample.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+
+const EventExample = () => {
+  return (
+    <div className='eventExample'>
+      <form>
+        <input type="text" placeholder='input anything here...' />
+        <button>Search</button>
+    </form>
+    </div>
+  )
+}
+
+export default EventExample
