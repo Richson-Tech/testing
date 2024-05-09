ada40d13e1456ed2da4f64ce84443e770d553458
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-05-09 20:16:18 +0100

    new commits

--- src/components/EventExample.tsx
+++ src/components/EventExample.tsx
@@ -1,14 +1,37 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 
+type UserType = {
+  sessionId: number;
+  name: string;
+};
+
 const [userName, setUsername] = useState("");
-const [user, setUser] = useState(null);
+const [user, setUser] = useState<UserType | null>(null);
 
 const EventExample = () => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handleCick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setUser({
+      name: userName,
+      sessionId: Math.random(),
+    });
+  };
+
   return (
     <div className="usestate">
-      <input type="text" placeholder="username" />
-      <button>Login</button>
+      {user ? (
+        `${user.name} logged in`
+      ) : (
+        <form>
+          <input type="text" placeholder="username" onChange={handleChange} />
+          <button onClick={handleCick}>Login</button>
+        </form>
+      )}
     </div>
   );
 };
