ab84b68c8a786a75c4f95ddb4092f37f09ef7897
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-05-10 20:04:43 +0100

    new commmits

--- src/components/EventExample.tsx
+++ src/components/EventExample.tsx
@@ -7,33 +7,10 @@ type UserType = {
 };
 
 const EventExample = () => {
-  const [userName, setUsername] = useState("");
-  const [user, setUser] = useState<UserType | null>(null);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-  };
-
-  const handleCick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setUser({
-      name: userName,
-      sessionId: Math.random(),
-    });
-  };
-
+ 
   return (
-    <div className="usestate">
-      {user ? (
-        `${user.name} logged in`
-      ) : (
-        <form>
-          <input type="text" placeholder="username" onChange={handleChange} />
-          <button onClick={handleCick}>Login</button>
-        </form>
-      )}
-      {/* illustration  */}
-      {user?.name}
+    <div className="useRefHook">
+     
     </div>
   );
 };
